5c47a2e70c4157ec84029264d9ff427117bf1649
majiyd <suleimanmajiyd@gmail.com>
2019-05-04 18:46:54 +0100

    contact form input event is dispatching contact form input actions

--- src/containers/AddNewContact.js
+++ src/containers/AddNewContact.js
@@ -1,9 +1,24 @@
 import React, { PureComponent } from 'react';
+import {connect} from 'react-redux'
+import {inputContactName, inputContactNumber} from '../redux/actions/actionCreators/contactActionCreators'
 import textStyles from '../css/components/Text.module.css'
 import formStyles from '../css/components/Form.module.css'
 import buttonStyles from '../css/components/Button.module.css'
 
+function mapStateToProps(){
+
+}
+const mapDispatchToProps = {
+  inputContactName,
+  inputContactNumber
+}
 class AddNewContact extends PureComponent {
+  handleInputContactName = () => {
+    this.props.inputContactName()
+  }
+  handleInputContactNumber = () => {
+    this.props.inputContactNumber()
+  }
   render() {
     return (
       <React.Fragment>
@@ -19,17 +34,19 @@ class AddNewContact extends PureComponent {
             className={formStyles.input} 
             placeholder='Name:'
             required
+            onInput={this.handleInputContactName}
           />
           <input 
             className={formStyles.input} 
             placeholder='Number:'
             required
+            onInput={this.handleInputContactNumber}
           />
-          <div><button className={buttonStyles.form_button}>Add Contact</button></div>
+          <button className={buttonStyles.form_button}>Add Contact</button>
         </form>
       </React.Fragment>
     );
   }
 }
 
-export default AddNewContact;
+export default connect(mapStateToProps, mapDispatchToProps)(AddNewContact);
